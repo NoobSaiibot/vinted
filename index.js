4564bb35f011e675f80286e7fc4dc28209cb5fbc
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-04-15 16:32:28 +0200

    added route/

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ const offerRoutes = require("./routes/offer");
 app.use(userRoutes);
 app.use(offerRoutes);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcolme on my server" });
+});
+
 app.all("*", (req, res) => {
   res.status(404).json({ message: "This route doesn't exist" });
 });
